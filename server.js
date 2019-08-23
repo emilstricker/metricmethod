563ab9ca700fb60e727949b88925d7a19352ecb5
Glitch (hello-express) <none>
2019-08-23 22:16:19 +0000

    🐣👋 Checkpoint ./package.json:257809/3886 ./server.js:257809/2325 ./todo.ejs:257809/1115 ./views/todo.ejs:257809/2167

--- server.js
+++ server.js
@@ -1,22 +1,54 @@
-// server.js
-// where your node app starts
+//jshint esversion:6
 
-// init project
-const express = require('express');
-const app = express();
+var express = require('express');
+var session = require('cookie-session'); // Loads the piece of middleware for sessions
+var bodyParser = require('body-parser'); // Loads the piece of middleware for managing the settings
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var sumGrams = 0;
+var dailyGrams = 1067;
+var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+/* Using the sessions */
+app.use(session({secret: 'todotopsecret'}))
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+/* If there is no to do list in the session,
+we create an empty one in the form of an array before continuing */
+.use(function(req, res, next){
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.todolist = [];
+        req.session.dailyGrams = 0;
+    }
+    next();
+})
 
-// listen for requests :)
+/* The to do list and the form are displayed */
+.get('/todo', function(req, res) {
+    res.render('todo.ejs', {todolist: req.session.todolist, sumGrams: sumGrams, dailyGrams: dailyGrams});
+})
+
+/* Adding an item to the to do list */
+.post('/todo/add/', urlencodedParser, function(req, res) {
+    if (req.body.newtodo != '') {
+        req.session.todolist.push(req.body.newtodo);
+    }
+    res.redirect('/todo');
+})
+
+/* Deletes an item from the to do list */
+.get('/todo/delete/:id', function(req, res) {
+    if (req.params.id != '') {
+        req.session.todolist.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
+})
+
+
+/* Redirects to the to do list if the page requested is not found */
+.use(function(req, res, next){
+    res.redirect('/todo');
+})
+
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
